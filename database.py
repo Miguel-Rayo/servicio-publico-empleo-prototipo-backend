3256c7eb984193340cc5a6fa1f5cadcd09941bba
Miguel-Rayo <135787598+Miguel-Rayo@users.noreply.github.com>
2026-03-19 12:26:57 -0500

    Adjust MySQL connection pool size and timeout settings
    Reduced the MySQL connection pool size from 5 to 3 and added connection timeout settings.

--- database.py
+++ database.py
@@ -20,6 +20,7 @@
 #     return pool.get_connection()
 
 # database.py
+# database.py
 import mysql.connector
 from mysql.connector import pooling
 import os
@@ -29,7 +30,7 @@ load_dotenv()
 
 pool = pooling.MySQLConnectionPool(
     pool_name="empleo_pool",
-    pool_size=5,
+    pool_size=3,
     host=os.getenv("DB_HOST"),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
@@ -38,6 +39,8 @@ pool = pooling.MySQLConnectionPool(
     charset="utf8mb4",
     ssl_disabled=False,
     ssl_verify_cert=False,
+    ssl_verify_identity=False,
+    connection_timeout=30,
 )
 
 def get_connection():
