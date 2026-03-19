33a0e0d9f4086a73381b1e6e281026b6a28550cf
Miguel Angel Rayo Romero <2miguelray2005@gmail.com>
2026-03-19 12:00:20 -0500

    agregar requirements y Procfile para Render

--- database.py
+++ database.py
@@ -1,4 +1,25 @@
 # database.py
+# import mysql.connector
+# from mysql.connector import pooling
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# pool = pooling.MySQLConnectionPool(
+#     pool_name="empleo_pool",
+#     pool_size=5,
+#     host=os.getenv("DB_HOST"),
+#     user=os.getenv("DB_USER"),
+#     password=os.getenv("DB_PASSWORD"),
+#     database=os.getenv("DB_NAME"),
+#     charset="utf8mb4"
+# )
+
+# def get_connection():
+#     return pool.get_connection()
+
+# database.py
 import mysql.connector
 from mysql.connector import pooling
 import os
@@ -13,7 +34,10 @@ pool = pooling.MySQLConnectionPool(
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
     database=os.getenv("DB_NAME"),
-    charset="utf8mb4"
+    port=int(os.getenv("DB_PORT", 3306)),
+    charset="utf8mb4",
+    ssl_disabled=False,
+    ssl_verify_cert=False,
 )
 
 def get_connection():
